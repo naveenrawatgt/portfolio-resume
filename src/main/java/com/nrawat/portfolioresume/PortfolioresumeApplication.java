2399340c052adc6f89643bcb758553b9f8ec828e
Naveen <naveenrawatgt@gmail.com>
2023-10-28 14:07:18 -0700

    initial commit

---
+++ src/main/java/com/nrawat/portfolioresume/PortfolioresumeApplication.java
@@ -0,0 +1,13 @@
+package com.nrawat.portfolioresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioresumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioresumeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nrawat/portfolioresume/PortfolioresumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrawat.portfolioresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioresumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
