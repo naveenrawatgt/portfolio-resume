b996870c733d29e98b9171cf66cfb9bfa7c814cd
Naveen <naveenrawatgt@gmail.com>
2023-10-28 14:38:44 -0700

    Customer API Added

---
+++ src/main/java/com/nrawat/portfolioresume/Model/Customer/Customer.java
@@ -0,0 +1,13 @@
+package com.nrawat.portfolioresume.Model.Customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
