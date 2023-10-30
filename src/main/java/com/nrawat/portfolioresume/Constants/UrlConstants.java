6eb4a80809931d15e46593b0696d55258cdee0f1
Naveen <naveenrawatgt@gmail.com>
2023-10-30 07:58:27 -0700

    Version Fixes

---
+++ src/main/java/com/nrawat/portfolioresume/Constants/UrlConstants.java
@@ -0,0 +1,5 @@
+package com.nrawat.portfolioresume.Constants;
+
+public interface UrlConstants {
+    public static String GET_CUSTOMER_LIST = "api/v1/customers";
+}
--- src/main/java/com/nrawat/portfolioresume/Controller/customer/CustomerController.java
+++ src/main/java/com/nrawat/portfolioresume/Controller/customer/CustomerController.java
@@ -1,22 +1,24 @@
 package com.nrawat.portfolioresume.Controller.customer;
 
+import com.nrawat.portfolioresume.Constants.UrlConstants;
 import com.nrawat.portfolioresume.Model.Customer.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/customers")
+//@RequestMapping("api/v1/customers")
 public class CustomerController {
-    private static final List<Customer> CUSTOMERS = List.of(
+    private List<Customer> CUSTOMERS = Arrays.asList(
             new Customer(1L, "john", "doe", "john@javawhizz.com"),
             new Customer(2L, "mary", "public", "mary@javawhizz.com"),
-            new Customer(3L, "elon", "musk","elon@javawhizz.com"),
-            new Customer(4L, "dunny","duncan","dunny@javawhizz.com")
+            new Customer(3L, "elon", "musk", "elon@javawhizz.com"),
+            new Customer(4L, "dunny", "duncan", "dunny@javawhizz.com")
     );
-    @GetMapping
+    @GetMapping(UrlConstants.GET_CUSTOMER_LIST)
     public List<Customer> findAllCustomers(){
         return CUSTOMERS;
     }
